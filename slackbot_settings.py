80682091129262e813566b535565a125a7c965a4
mitsumaru3291 <56405202+mitsumaru3291@users.noreply.github.com>
2020-02-25 18:18:16 +0900

    Add files via upload

---
+++ slackbot_settings.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+# botアカウントのトークンを指定
+API_TOKEN = "xxxxxxx-xxxxxxxxxxxxx-xxxxxxxxxxxxx"
+
+# デフォルトのリプ
+DEFAULT_REPLY = "カウントしたくて死にそうです"
+
+# プラグインスクリプトを置いてあるサブディレクトリ名のリスト
+PLUGINS = ['plugins']
